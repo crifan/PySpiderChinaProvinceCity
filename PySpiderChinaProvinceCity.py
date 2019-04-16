7406ee33ade57238319357f639a09b10668cfebc
naturling_crifan <crifan.li@naturling.com>
2019-04-16 10:25:16 +0800

    1. first version

---
+++ PySpiderChinaProvinceCity.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Created on 2019-04-15 14:56:12
+# Project: PySpiderChinaProvinceCity
+
+from pyspider.libs.base_handler import *
+
+import os
+import json
+import codecs
+import base64
+import gzip
+import copy
+import time
+import re
+import csv
+# import datetime
+from datetime import datetime, timedelta
+
+
+######################################################################
+# Const
+######################################################################
+
+OutputRoot = "/Users/crifan/dev/dev_root/crifan/PySpiderChinaProvinceCity/output"
+
+constProvinceListFullpath = os.path.join(OutputRoot, "provinceList.json")
+
+constCityListNamePattern = "cityList_%s_%s.json"
+
+constUserAgentMacChrome = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
+
+######################################################################
+# Config & Settings
+######################################################################
+
+
+
+######################################################################
+# Global Varaibles
+######################################################################
+
+gProvinceCityList = []
+
+######################################################################
+# Common Util Functions
+######################################################################
+
+def saveJsonToFile(fullFilename, jsonValue):
+    """save json dict into file"""
+    with codecs.open(fullFilename, 'w', encoding="utf-8") as jsonFp:
+        json.dump(jsonValue, jsonFp, indent=2, ensure_ascii=False)
+        print("Complete save json %s" % fullFilename)
+
+def loadJsonFromFile(fullFilename):
+    """load and parse json dict from file"""
+    with codecs.open(fullFilename, 'r', encoding="utf-8") as jsonFp:
+        jsonDict = json.load(jsonFp)
+        print("Complete load json from %s" % fullFilename)
+        return jsonDict
+
+######################################################################
+# Project Specific Functions
+######################################################################
+
+
+######################################################################
+# Main
+######################################################################
+
+class Handler(BaseHandler):
+    crawl_config = {
+        "connect_timeout": 100,
+        "timeout": 600,
+        "retries": 15,
+        "headers": {
+            "User-Agent": constUserAgentMacChrome,
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Content-Type": "application/json",
+            "Origin": "http://www.dianping.com",
+            # "X-Requested-With": "XMLHttpRequest",
+        }
+    }
+
+    #----------------------------------------
+    # Util Functions
+    #----------------------------------------
+
+    #----------------------------------------
+    # Crawl Logic
+    #----------------------------------------
+
+
+    def on_start(self):
+        # init province city list
+        # self.dowloadProvinceCity()
+        self.mergeProvinceCity()
+
+    def mergeProvinceCity(self):
+        gProvinceCityList = loadJsonFromFile(constProvinceListFullpath)
+
+        for eachProvoince in gProvinceCityList:
+            curCityListFilename = constCityListNamePattern % (eachProvoince["provinceId"], eachProvoince["provinceName"])
+            curCityListFullpath = os.path.join(OutputRoot, curCityListFilename)
+            curCityList = loadJsonFromFile(curCityListFullpath)
+            eachProvoince["currentNodeLevel"] = 1
+            eachProvoince["children"] = []
+
+            """
+            {
+                "activeCity": True,
+                "appHotLevel": 1,
+                "cityAbbrCode": "SUZ",
+                "cityAreaCode": "0512",
+                "cityEnName": "suzhou",
+                "cityId": 6,
+                "cityLevel": 2,
+                "cityName": "苏州",
+                "cityOrderId": 4888,
+                "cityPyName": "suzhou",
+                "directURL": "",
+                "gLat": 31.297779,
+                "gLng": 120.585586,
+                "overseasCity": False,
+                "parentCityId": 0,
+                "provinceId": 10,
+                "scenery": False,
+                "tuanGouFlag": 1
+            },
+
+            {
+                "activeCity": True,
+                "appHotLevel": 1,
+                "cityAbbrCode": "TC",
+                "cityAreaCode": "0512",
+                "cityEnName": "taicang",
+                "cityId": 420,
+                "cityLevel": 100,
+                "cityName": "太仓",
+                "cityOrderId": 4994,
+                "cityPyName": "taicang",
+                "directURL": "",
+                "gLat": 31.45868,
+                "gLng": 121.13003,
+                "overseasCity": False,
+                "parentCityId": 6,
+                "provinceId": 10,
+                "scenery": False,
+                "tuanGouFlag": 1
+            },
+            """
+
+            # extract main city
+            for eachCityItem in curCityList:
+                toDelKeyList = [
+                    "activeCity",
+                    "appHotLevel",
+                    "directURL",
+                    "overseasCity",
+                    "tuanGouFlag",
+                    "scenery",
+                ]
+                for eachKeyToDel in toDelKeyList:
+                    if eachKeyToDel in eachCityItem:
+                        eachCityItem.pop(eachKeyToDel)
+                        # del eachCityItem[eachKeyToDel]
+
+                parentCityId = eachCityItem["parentCityId"]
+                print("parentCityId=%s" % parentCityId)
+                if parentCityId == 0:
+                    # is main city
+                    eachCityItem["currentNodeLevel"] = 2
+                    eachCityItem["children"] = []
+                    eachProvoince["children"].append(eachCityItem)
+
+            # extract sub city
+            for eachCityItem in curCityList:
+                print("eachCityItem=%s" % eachCityItem)
+                parentCityId = eachCityItem["parentCityId"]
+                print("parentCityId=%s" % parentCityId)
+                if parentCityId > 0:
+                    # is sub city
+                    eachCityItem["currentNodeLevel"] = 3
+                    # add to  main city
+                    curMainCityList = eachProvoince["children"]
+                    for earchMainCity in curMainCityList:
+                        if earchMainCity["cityId"] == parentCityId:
+                            earchMainCity["children"].append(eachCityItem)
+                else:
+                    eachCityItem.pop("parentCityId")
+
+        provinceCityListFilename = "provinceCityList_%s.json" % datetime.now().strftime("%Y%m%d")
+        provinceCityListFullpath = os.path.join(OutputRoot, provinceCityListFilename)
+        saveJsonToFile(provinceCityListFullpath, gProvinceCityList)
+
+    def dowloadProvinceCity(self):
+        getAllDomesticProvinceUrl = "http://www.dianping.com/ajax/citylist/getAllDomesticProvince"
+        self.crawl(
+            getAllDomesticProvinceUrl,
+            method="POST",
+            data="",
+            callback=self.getAllDomesticProvinceCallback,
+        )
+
+    def getAllDomesticProvinceCallback(self, response):
+        respUrl = response.url
+        print("respUrl=%s" % respUrl)
+        respContent = response.content
+        print("respContent=%s" % respContent)
+        respJson = response.json
+        print("respJson=%s" % respJson)
+        provinceList = respJson["provinceList"]
+
+        for eachProvince in provinceList:
+            provinceIdStr = eachProvince["provinceId"]
+            provinceIdInt = int(provinceIdStr)
+            eachProvince["provinceId"] = provinceIdInt
+
+        saveJsonToFile(constProvinceListFullpath, provinceList)
+
+        # get all cities for each province
+        getCityUrl = "http://www.dianping.com/ajax/citylist/getDomesticCityByProvince"
+        for eachProvince in provinceList:
+            print("eachProvince=%s" % eachProvince)
+            provinceIdInt = eachProvince["provinceId"]
+            paramDict = {
+                "provinceId": provinceIdInt,
+            }
+            paramDictStr = json.dumps(paramDict)
+
+            provinceDict = eachProvince
+            provinceDict["provinceId"] = provinceIdInt
+            gProvinceCityList.append(provinceDict)
+
+            # add itag and hash value for url to force re-crawl when POST url not changed
+            timestampStr = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+            # fakeItag = "%s_%s" % (provinceIdStr, timestampStr)
+            urlWithHash = "%s#%s_%s" % (getCityUrl, provinceIdStr, timestampStr)
+
+            self.crawl(
+                # getCityUrl,
+                urlWithHash,
+                # itag=fakeItag,
+                method="POST",
+                # data=paramDict,
+                data=paramDictStr,
+                callback=self.getCityCallback,
+                save=provinceDict,
+            )
+
+    def getCityCallback(self, response):
+        provinceDict = response.save
+        print("provinceDict=%s" % provinceDict)
+        respUrl = response.url
+        print("respUrl=%s" % respUrl)
+        respJson = response.json
+        print("respJson=%s" % respJson)
+        cityList = respJson["cityList"]
+        print("cityList=%s" % cityList)
+
+        curCityListFilename = constCityListNamePattern % (provinceDict["provinceId"], provinceDict["provinceName"])
+        curCityListFullpath = os.path.join(OutputRoot, curCityListFilename)
+        saveJsonToFile(curCityListFullpath, cityList)
---
+++ README.md
@@ -0,0 +1,180 @@
+# PySpiderChinaProvinceCity
+
+最后更新：`20190416`
+
+## 项目目的
+
+* 演示如何用[PySpider](http://book.crifan.com/books/python_spider_pyspider/website)从[大众点评网站](http://www.dianping.com/citylist)爬取**中国省市区**的数据信息
+* 生成得到**中国省市区数据**
+  * 供别人或自己以后使用
+
+## git仓库
+
+crifan/PySpiderChinaProvinceCity
+
+https://github.com/crifan/PySpiderChinaProvinceCity
+
+## 如何运行项目得到数据
+
+### 初始化PySpider环境
+
+```bash
+pipenv install
+pipenv shell
+```
+
+注意：Mac中`pipenv install`时如果报错
+
+```bash
+__main__.ConfigurationError: Curl is configured to use SSL, but we have not been able to determine which SSL backend it is using. Please see PycURL documentation for how to specify the SSL backend manually
+```
+
+可以参考：
+
+```bash
+pipenv shell
+pip3 uninstall pycurl
+export PYCURL_SSL_LIBRARY=openssl
+export LDFLAGS=-L/usr/local/opt/openssl/lib;export CPPFLAGS=-I/usr/local/opt/openssl/include;pip3 install pycurl --compile --no-cache-dir
+```
+
+详见：[【已解决】pipenv虚拟环境中用pip安装pyspider出错：\_\_main\_\_.ConfigurationError: Curl is configured to use SSL, but we have not been able to determine which SSL backend it is using](http://www.crifan.com/pipenv_virtual_environment_pip_install_pyspider_error_main_configurationerror_curl_is_configured_to_use_ssl_but_we_have_not_been_able_to_determine_which_ssl_backend_it_is_using)
+
+### 拷贝PySpider代码
+
+```bash
+pyspider
+```
+
+默认用`5000`端口，然后打开：
+
+http://0.0.0.0:5000/
+
+再去：
+
+`Create` -> `PySpiderChinaProvinceCity` -> `Create` -> 把`PySpiderChinaProvinceCity.py`粘贴进去->点击`save`，即可保存代码。
+
+再去把`OutputRoot`改为你要保存数据的目录。
+
+### 运行PySpider代码
+
+先去把代码改为：
+
+```python
+        self.dowloadProvinceCity()
+        # self.mergeProvinceCity()
+```
+
+然后去运行：
+
+界面中把`status`改为`RUNNING`，再点击`Run`
+
+即可在`output`文件夹中生成：
+
+* `provinceList.json`：省的列表信息
+* `cityList_{cityId}_{cityName}.json`：每个省的城市信息，包括市和市下属的区/县
+
+```bash
+➜  output git:(master) ✗ ll
+total 2272
+-rw-r--r--  1 crifan  staff    30K  4 16 09:32 cityList_10_江苏.json
+-rw-r--r--  1 crifan  staff    35K  4 16 09:32 cityList_11_浙江.json
+-rw-r--r--  1 crifan  staff    36K  4 16 09:32 cityList_12_安徽.json
+-rw-r--r--  1 crifan  staff    30K  4 16 09:32 cityList_13_福建.json
+-rw-r--r--  1 crifan  staff    41K  4 16 09:32 cityList_14_江西.json
+-rw-r--r--  1 crifan  staff    50K  4 16 09:31 cityList_15_山东.json
+-rw-r--r--  1 crifan  staff    56K  4 16 09:32 cityList_16_河南.json
+-rw-r--r--  1 crifan  staff    36K  4 16 09:32 cityList_17_湖北.json
+-rw-r--r--  1 crifan  staff    44K  4 16 09:32 cityList_18_湖南.json
+-rw-r--r--  1 crifan  staff    43K  4 16 09:32 cityList_19_广东.json
+-rw-r--r--  1 crifan  staff   2.6K  4 16 09:31 cityList_1_北京.json
+-rw-r--r--  1 crifan  staff    39K  4 16 09:32 cityList_20_广西.json
+-rw-r--r--  1 crifan  staff   8.6K  4 16 09:32 cityList_21_海南.json
+-rw-r--r--  1 crifan  staff    13K  4 16 09:32 cityList_22_重庆.json
+-rw-r--r--  1 crifan  staff    72K  4 16 09:32 cityList_23_四川.json
+-rw-r--r--  1 crifan  staff    39K  4 16 09:32 cityList_24_贵州.json
+-rw-r--r--  1 crifan  staff    59K  4 16 09:32 cityList_25_云南.json
+-rw-r--r--  1 crifan  staff    35K  4 16 09:32 cityList_26_西藏.json
+-rw-r--r--  1 crifan  staff    42K  4 16 09:32 cityList_27_陕西.json
+-rw-r--r--  1 crifan  staff    36K  4 16 09:32 cityList_28_甘肃.json
+-rw-r--r--  1 crifan  staff    20K  4 16 09:32 cityList_29_青海.json
+-rw-r--r--  1 crifan  staff   2.5K  4 16 09:32 cityList_2_天津.json
+-rw-r--r--  1 crifan  staff   7.9K  4 16 09:32 cityList_30_宁夏.json
+-rw-r--r--  1 crifan  staff    45K  4 16 09:31 cityList_31_新疆.json
+-rw-r--r--  1 crifan  staff   479B  4 16 09:32 cityList_32_香港.json
+-rw-r--r--  1 crifan  staff   469B  4 16 09:32 cityList_33_澳门.json
+-rw-r--r--  1 crifan  staff   8.8K  4 16 09:32 cityList_34_台湾.json
+-rw-r--r--  1 crifan  staff    64K  4 16 09:31 cityList_3_河北.json
+-rw-r--r--  1 crifan  staff    47K  4 16 09:32 cityList_4_山西.json
+-rw-r--r--  1 crifan  staff    41K  4 16 09:32 cityList_5_内蒙古.json
+-rw-r--r--  1 crifan  staff    25K  4 16 09:32 cityList_6_辽宁.json
+-rw-r--r--  1 crifan  staff    22K  4 16 09:31 cityList_7_吉林.json
+-rw-r--r--  1 crifan  staff    35K  4 16 09:32 cityList_8_黑龙江.json
+-rw-r--r--  1 crifan  staff   891B  4 16 09:32 cityList_9_上海.json
+-rw-r--r--  1 crifan  staff   2.6K  4 16 09:31 provinceList.json
+```
+
+然后再去把代码改为：
+
+```python
+        # self.dowloadProvinceCity()
+        self.mergeProvinceCity()
+```
+
+去调试，点击`run`，即可生成
+
+* `provinceCityList_20190416.json`：包含了合并后的，所有的`中国省市区`的数据。
+
+## 中国省市区数据结构简介
+
+### 数据在哪里
+
+在`OutputRoot`变量定义的目录下的`provinceCityLis_YYYYMMDD.json`
+
+比如：[中国省市区数据列表 provinceCityList_20190416.json](https://github.com/crifan/PySpiderChinaProvinceCity/blob/master/output/provinceCityList_20190416.json)
+
+### 数据结构关系
+
+上述的省市区的数据内部的字段含义和逻辑层次关系：
+
+```bash
+
+  {
+    "areaId": 3, # 地区编号
+    "provinceId": 10, # 省份编号
+    "provinceName": "江苏", # 省份名
+    "currentNodeLevel": 1, # 当前节点层级：1=省，2=市，3=区/县
+    "children": [
+      {
+        "cityAbbrCode": "SUZ", # 市缩写编码
+        "cityAreaCode": "0512", # 市地区编码=邮政编码
+        "cityEnName": "suzhou", # 市英文名称
+        "cityId": 6, # 市编号
+        "cityLevel": 2, # 几线城市：1=一线，2=二线，3=三线，4=四线，100=五线？
+        "cityName": "苏州", # 市中文名
+        "cityOrderId": 4888, # 市排序编号
+        "cityPyName": "suzhou", # 市拼音名
+        "gLat": 31.297779, # 地理信息：经度
+        "gLng": 120.585586, # 地理信息：维度
+        "provinceId": 10, # 所属省份编号
+        "currentNodeLevel": 2, # 当前节点层级：1=省，2=市，3=区/县
+        "children": [
+          {
+            "cityAbbrCode": "CS",
+            "cityAreaCode": "0512",
+            "cityEnName": "changshu",
+            "cityId": 417,
+            "cityLevel": 100,
+            "cityName": "常熟",
+            "cityOrderId": 712,
+            "cityPyName": "changshu",
+            "gLat": 31.65355,
+            "gLng": 120.75239,
+            "parentCityId": 6, # 父亲城市=市 的编号
+            "provinceId": 10,
+            "currentNodeLevel": 3 # 当前节点层级：1=省，2=市，3=区/县
+          },
+          ...
+      },
+      ...
+```
